55c8fd53147ef7c6d976d427b5363f170a8ef72a
youngmasterhao <apaul.mcsi@gmail.com>
2025-04-23 08:26:44 +0800

    home.js

--- my-website/js/home.js
+++ my-website/js/home.js
@@ -1,4 +1,4 @@
-const API_KEY = 'a1e72fd93ed59f56e6332813b9f8dcae';
+const API_KEY = 'db08512c272ba62a1b1f01e25f1c84b7';
     const BASE_URL = 'https://api.themoviedb.org/3';
     const IMG_URL = 'https://image.tmdb.org/t/p/original';
     let currentItem;
